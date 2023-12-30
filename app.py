489ad8cf508522c48446c08b1064be4bbf59eeaa
Adi-Ida404 <146981927+Adi-Ida404@users.noreply.github.com>
2023-12-30 18:37:39 +0000

    made individual jobpage

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import engine
-from sqlalchemy import text
+from flask import Flask, jsonify, render_template
+from database import load_job_from_db
 
 app = Flask(__name__)
 JOBS=[
@@ -16,7 +15,7 @@ JOBS=[
     'id':2,
     'title':'App Developer',
     'location':'San Francisco, USA', 
-    'Salary':'$ 500,000 PA',
+    'Salary':'$ 500,000',
     'skills':'Python, Django, Flask',
     'require':'2 yrs+ Experience in Relevant Field'
   },
@@ -41,6 +40,11 @@ JOBS=[
 @app.route("/")
 def home():
   return render_template("home.html", jobs=JOBS)
+
+@app.route("/jobs/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  return render_template("jobpage.html", job=job)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,17 +1,33 @@
 import sqlalchemy
-from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey, DateTime
+from sqlalchemy import create_engine, text
 import os
 
-db_connect_url=os.environ['DB_CONNECTION_URL']
+db_connect_url = os.environ['DB_CONNECTION_URL']
 
 engine = create_engine(    
-  db_connect_url,
-  connect_args=
-  {
-  "ssl": {
-    "ssl_ca": "/etc/ssl/cert.pem"
+    db_connect_url,
+    connect_args={
+        "ssl": {
+            "ssl_ca": "/etc/ssl/cert.pem"
+        }
     }
-  })
+)
+
+try:
+
+    def load_job_from_db(id):
+        with engine.connect() as conn:
+            result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"), {"val": id})
+            rows = result.all()
+            if len(rows) == 0:
+                return None
+            else:
+                return rows[0]._asdict()
+              
+except Exception as e:
+    print(f"Error: {e}")
+
+  
 
 
   
